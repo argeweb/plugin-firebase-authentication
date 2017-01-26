e139c9d61c3c4805ddffb2b59f4a0d50f20edf09
cwen0708 <cwen0708@gmail.com>
2017-01-26 20:35:18 +0800

    scaffold 欄位設定方式改變

--- controllers/firebase_authentication.py
+++ controllers/firebase_authentication.py
@@ -20,8 +20,8 @@ class FirebaseAuthentication(Controller):
         pagination_limit = 10
 
     class Scaffold:
-        display_properties_in_list = ('name', 'title', 'is_enable', 'category')
-        hidden_properties_in_edit = ('name',)
+        display_in_list = ('name', 'title', 'is_enable', 'category')
+        hidden_in_form = ('name',)
 
     @route
     @route_menu(list_name=u'backend', text=u'Firebase 驗証設定', sort=9903, group=u'系統設定')
